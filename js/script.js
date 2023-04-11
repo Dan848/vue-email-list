d7813c405d5419cb40774b97d85b1b635473e11e
Dan848 <daniele.ceravolo15@gmail.com>
2023-04-11 18:56:57 +0200

    Create list

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            titolo: "Esperimenti API",
+            listMail: [],
+        }
+    },
+    methods: {
+        getData(){
+            for (let i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res) => {
+                    this.listMail.push(res.data.response);
+
+                })
+            }
+            console.log(this.listMail)
+        }
+    },
+    mounted(){
+        console.log(this.listMail)
+    }
+}).mount("#app")
